f891c14ab4758578e6f803475f2989855c619294
Adewale Alimi <alimiadewale68@gmail.com>
2022-12-01 09:32:45 -0800

    manipulating DOM with javascript

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+    
+    const quoteContainer = document.getElementById('quote-container');
+    const quoteText = document.getElementById('quote');
+    const authorText = document.getElementById('author');
+    const twitterBtn = document.getElementById('twitter');
+    const newQuoteBtn = document.getElementById('new-quote');
+    
+
+
 // Get Quotes From API
     let apiQuotes = [];
 
@@ -5,7 +14,19 @@
     function newQuote() {
         // Pick a random quote from apiQuotes array
         const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-        console.log(quote);
+        // Check if Author field is blank and replace it with 'Unknown'
+        if (!quote.author)  {
+            authorText.textContent = 'Unknown';
+        } else {
+            authorText.textContent = quote.author;
+        }
+        // Check Quote length to determine styling
+        if (quote.text.length > 120 ) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.textContent = quote.text;
 
     }
         //Get Quotes fROM API
@@ -18,7 +39,19 @@
         } catch(error) {
             // Catch Error Here
         } 
-    }
+    }  
+    
+        // Tweet Quote
+        function tweetQuote() {
+            const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.
+            textContent} - ${authorText.textContent}`;
+            window.open(twitterUrl, '_blank');
+        }
+
+        //  Event Listeners
+        newQuoteBtn.addEventListener('click', newQuote);
+        twitterBtn.addEventListener('click',  tweetQuote);
+     
 
     // On Load
     getQuotes();
